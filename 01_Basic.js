403cea96d0ff47b4c5290e7e0aecc149e77a3d3a
swapnilkuthe <swapnilkuthe20@gmail.com>
2024-10-31 21:05:19 +0530

    Basic practice supersimple dev

--- 01_Basic.js
+++ 01_Basic.js
@@ -1,1 +1,11 @@
-console.log("Welcome");
+// console.log("Welcome");
+
+// console.log(Math.round(2.5));    // 3
+// console.log(Math.round(2.6));    // 3
+// console.log(Math.round(2.4));    // 2
+
+// console.log((2097 + 799)*0.1/100);
+// console.log(Math.round(2097 + 799)*0.1)/100;
+
+console.log(18.50 * 7.50);
+
